3ef340d107b429b6bdb176b09bca3e2959cb0782
unknown <arthur.hoodwin@gmail.com>
2025-11-26 10:55:24 +0300

    Подтягивание globals.css

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import './globals.css'
+
 export const metadata = { title: "Трекер задач" };
 export default function RootLayout({ children }) {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: ["./app/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
